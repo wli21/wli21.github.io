a09ef571c695e6e47dfee532800caf6c2ee4ccdf
wli21 <weijia.l.1994@gmail.com>
2016-11-10 21:40:58 -0500

    add resources

--- p3/rasterize.js
+++ p3/rasterize.js
@@ -637,25 +637,7 @@ function renderModels() {
         gl.uniformMatrix4fv(pvmMatrixULoc, false, hpvmMatrix); // pass in handed project view model matrix
 
 		//texture
-		neheTexture = gl.createTexture();
-	
-		gl.bindTexture(gl.TEXTURE_2D, neheTexture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-              new Uint8Array([255, 0, 0, 255])); // red
-	
-		neheTexture.image = new Image();
-		neheTexture.image.src = "resources/nehe.gif";
-	
-		neheTexture.image.onload = function () {
-			gl.bindTexture(gl.TEXTURE_2D, neheTexture);
-			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, neheTexture.image);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-			gl.bindTexture(gl.TEXTURE_2D, null);
-		
-		
-		}
+		initTexture();
 		
 		
 		
